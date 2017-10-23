69f2f8ea68ed7a9ca8ed25ff9c173ad93f7925b3
Jérôme Hilaire <hilj@mcc-berlin.net>
2017-10-23 16:38:39 +0200

    Update (bis)

---
+++ main.R
@@ -0,0 +1,18 @@
+#### USER SECTION ######################################
+
+
+
+
+#### INITIALISE ########################################
+#source("scripts/init.R")
+
+
+
+#### RUN SCRIPTS #######################################
+# 
+#source("scripts/main_history.R")
+
+#source("scripts/main_qualitativeAnalysis.R")
+
+
+source("scripts/main_quantitativeAnalysis.R")
